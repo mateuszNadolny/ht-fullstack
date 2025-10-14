85ff87fa10983acdbedf81519a8333d0f20d18f3
mateuszNadolny <mateusznadolny00@gmail.com>
2025-10-14 21:05:29 +0200

    feat: Add rate limiting middleware to enhance API security
    - Integrated express-rate-limit to limit requests to the API, allowing a maximum of 100 requests per 15 minutes per IP.
    - Updated server.ts to include the rate limiting middleware, improving overall application security against abuse.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import habitRoutes from "./routes/habitRoutes.js";
 import recordRoutes from "./routes/recordRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -12,10 +13,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5003;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per 15 min
+  message: {
+    status: 429,
+    error: "Too many requests, please try again later.",
+  },
+  standardHeaders: true, // Return rate limit info in headers
+  legacyHeaders: false, // Disable the X-RateLimit headers
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(limiter);
 
 app.use("/api/habit", habitRoutes);
 
